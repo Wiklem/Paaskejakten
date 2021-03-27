02d13c1daef1499db589757d79bfc9b02e3b5f1b
Wiklem <admin@wiklem.no>
2021-03-27 17:46:40 +0100

    Remote url

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const functionUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001/paaskejakten/us-central1/"
-    : "https://us-central1-wiklem-8a13e.cloudfunctions.net/";
+    : "https://us-central1-paaskejakten.cloudfunctions.net/";
 
 export const apiStates = {
   LOADING: "LOADING",
